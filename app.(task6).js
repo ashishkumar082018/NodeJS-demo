ac575276f82e3108698636455d9aa59a42af3151
ashishkumar082018 <ashishkumar082018@gmail.com>
2024-04-10 08:07:31 +0530

    task 7

--- app.(task6).js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-
-const server = http.createServer((req, res) => {
-    console.log("Ashish kumar");
-})
-
-server.listen(4000);
---
+++ app.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log("Ashish kumar");
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node JS Test</title></head>');
+    res.write('<body>');
+    if (req.url === '/') {
+        res.write('<h1>Welcome to my Node Js project</h1>');
+    }
+    if (req.url === '/home') {
+        res.write('<h1>Welcome home</h1>');
+    }
+    if (req.url === '/about') {
+        res.write('<h1>Welcome to About Us page</h1>');
+    }
+    if (req.url === '/node') {
+        res.write('<h1>Welcome to my Node Js project</h1>');
+    }
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+})
+
+server.listen(3000);
