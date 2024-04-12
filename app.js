7c87dbb8f527fbc9e079c1b09034086659f55677
ashishkumar082018 <ashishkumar082018@gmail.com>
2024-04-12 15:13:58 +0530

    Task 2 Intro to Middlewares

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+const { text } = require('body-parser');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-app.use((req, res,next) =>{
-    console.log("MiddleWare 1");
-    next();
+app.use(bodyParser.urlencoded({extended : "false"}));
+
+app.use('/add-product', (req, res,next) =>{
+    res.send('<form action="/product" method="POST">' +
+    '<input type="text" name="Title" placeholder="Enter Product Title">' +
+    '<input type="text" name="Size" placeholder="Enter Product Size">' +
+    '<button type="submit">Add Product</button>' +
+ '</form>')
 });
 
-app.use((req, res,next) =>{
-    console.log("MiddleWare 2");
-    res.send("<h1> Hello from Express!</h1>")
+app.use('/product', (req, res,next) =>{
+    const title = req.body.Title;
+    const size = req.body.Size;
+    console.log('Product Title:', title);
+    console.log('Product Size:', size);
+    res.redirect('/')
 });
+
+app.use('/', (req, res,next) =>{
+    res.send("<h1> Hello from express js</h1>")
+});
+
+
 
 app.listen(3000);
